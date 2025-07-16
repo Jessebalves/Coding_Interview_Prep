c5393492629e364a00ca645068796f46579e0695
Jessebalves <152413074+Jessebalves@users.noreply.github.com>
2025-07-16 13:47:29 -0400

    Update maxDepth-Trees.py

--- LC_Solutions/maxDepth-Trees.py
+++ LC_Solutions/maxDepth-Trees.py
@@ -1,3 +1,9 @@
+# Maximum Depth for a binary tree problem
+# Given the root of a binary tree, return its maximum depth.
+
+# A binary tree's maximum depth is the number of nodes along the longest path from the root node
+# down to the farthest leaf node.
+#--------------------------------------------------------->
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
