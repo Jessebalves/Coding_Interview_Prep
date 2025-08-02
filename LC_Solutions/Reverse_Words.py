072c4114d74df763436ecac39a1c19bc26bb3a3d
Jessebalves <152413074+Jessebalves@users.noreply.github.com>
2025-08-02 12:00:29 -0400

    Update Reverse_Words.py

--- LC_Solutions/Reverse_Words.py
+++ LC_Solutions/Reverse_Words.py
@@ -7,6 +7,25 @@
 #Note that s may contain leading or trailing spaces or multiple spaces between two words. 
 #The returned string should only have a single space separating the words. Do not include any extra spaces.
 
+#class Solution(object):
+#    def reverseWords(self, s):
+#        """
+#        :type s: str
+#        :rtype: str
+#        """
+#        new_string = ""
+#        new_words = s.split()
+#        reversed_words = new_words[::-1]
+#        print(reversed_words)
+
+#        for i in range(len(reversed_words)):
+#            if i+1 != len(reversed_words):
+#                new_string += reversed_words[i] + " "
+#            else:
+#                new_string += reversed_words[i]
+
+#        return new_string
+
 class Solution(object):
     def reverseWords(self, s):
         """
@@ -15,13 +34,7 @@ class Solution(object):
         """
         new_string = ""
         new_words = s.split()
-        reversed_words = new_words[::-1]
-        print(reversed_words)
+        for value in new_words[::-1]:
+            new_string += value + " "
 
-        for i in range(len(reversed_words)):
-            if i+1 != len(reversed_words):
-                new_string += reversed_words[i] + " "
-            else:
-                new_string += reversed_words[i]
-
-        return new_string
+        return new_string[0:-1]
